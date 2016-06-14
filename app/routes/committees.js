381f2e864eba268ed2a9d89a6f0cd84b2dc13142
Kevin Deganos and Ming Wen <ming@>
2016-06-14 09:41:18 -0700

    format tables and display committess

--- app/routes/committees.js
+++ app/routes/committees.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    var url = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&apikey=d844eddef7b34e9a865556ff0f16a2da';
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+   }
 });
