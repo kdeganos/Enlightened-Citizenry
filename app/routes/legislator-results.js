45461d89872c19639333b4bb0ea9564be7aaf054
Kevin Deganos and Ming Wen <kevin@>
2016-06-14 16:22:22 -0700

    add sponsor detail page and display zip code on legislators page

--- app/routes/legislator-results.js
+++ app/routes/legislator-results.js
@@ -3,8 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=d844eddef7b34e9a865556ff0f16a2da&zip=' + params.zip;
-    return Ember.$.getJSON(url).then(function(responseJSON) {
-      return responseJSON.results;
+    console.log(params.zip);
+    return Ember.RSVP.hash({
+      legislatorsZip: Ember.$.getJSON(url).then(function(responseJSON) {
+          console.log(responseJSON.results);
+          return responseJSON.results;
+        }),
+      zipParam: params.zip
     });
    }
 });
